a9a5af43f65f32f8fa8335f1fe177d2486f74d97
Ain Ghazal <ainghazal42@gmail.com>
2022-11-20 23:03:38 +0100

    organize in folders

--- archive.sh
+++ archive.sh
@@ -2,7 +2,7 @@
 FILE=$1
 DEST=$2
 mkdir -p $DEST
-TS=`./check_measurements $1 | grep START | cut -f 2,3,4 -d ':' | sed -e 's/^[ \t]*//' | sed -e 's/[ ]/_/g'`
+TS=`./check_measurements.sh $1 | grep START | cut -f 2,3,4 -d ':' | sed -e 's/^[ \t]*//' | sed -e 's/[ ]/_/g'`
 HOSTNAME=$(hostname)
 FILENAME="$2/${HOSTNAME}-${TS}.jsonl"
 cp $1 $FILENAME
