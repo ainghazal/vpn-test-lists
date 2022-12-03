bef99761d50c435a872d76937227226cca8000d9
Ain Ghazal <ainghazal42@gmail.com>
2022-12-03 21:19:52 +0530

    add dummy files

--- run-wireguard.sh
+++ run-wireguard.sh
@@ -4,6 +4,6 @@ BACKEND=https://ams-pg-test.ooni.org
 cd /root/ooni
 for desc in privaterun/*.json;
 do 
-	./miniooni.vpn -y -f $desc -o report-wg.jsonl oonirun;
+	./miniooni.wg -y -f $desc -o report-wg.jsonl oonirun;
 	#./miniooni.vpn -y -f $desc --probe-services $BACKEND oonirun;
 done
