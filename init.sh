3e9df8eb2502b817fff61a410b63542be7b1ed39
Ain Ghazal <ainghazal42@gmail.com>
2023-01-03 13:19:14 +0100

    permissions

--- init.sh
+++ init.sh
@@ -5,5 +5,8 @@ adduser $USER
 mkdir -p $HOME/ooni/oonirun
 mkdir -p $HOME/ooni/privaterun
 mkdir -p $HOME/ooni/store
-mkdir -p $HOME/.miniooni
+mkdir -p $HOME/.miniooni/vpn
+mkdir -p $HOME/.ssh
 chown -R $USER:$USER $HOME/ooni
+chown -R $USER:$USER $HOME/.ssh
+chown -R $USER:$USER $HOME/.miniooni
