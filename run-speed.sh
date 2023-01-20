a599fa257ca833c3cecc8173792d618d2fa9cf24
Ain Ghazal <ainghazal42@gmail.com>
2023-01-20 23:28:23 +0100

    update cert

--- run-speed.sh
+++ run-speed.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
-./miniooni -f oonirun.exp/013-riseup-obfs4-proxy-speedtest.json -o report-speedtest.jsonl --repeat-every=120 oonirun
+while :
+do
+        ./miniooni -f oonirun.exp/013-riseup-obfs4-proxy-speedtest.json -o report-speedtest.jsonl oonirun
+        sleep 120
+done
